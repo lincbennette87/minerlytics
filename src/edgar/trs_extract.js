657f34d27dcafe78ad0912837a96918b4b26a296
Vishakh Sahni <vishakhedu@gmail.com>
2026-03-02 23:51:29 -0500

    Update trs_extract.js

--- src/edgar/trs_extract.js
+++ src/edgar/trs_extract.js
@@ -1,6 +1,6 @@
 import fs from "node:fs";
 import path from "node:path";
-import cheerio from "cheerio";
+import * as cheerio from "cheerio";
 
 const OUT_DIR = "public/data";
 const REPORT_DIR = "public/data/trs_reports";
