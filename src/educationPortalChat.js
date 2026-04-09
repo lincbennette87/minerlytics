a163ff9973dcc7ff7f9119fce77bb079922db5cd
Vishakh Sahni <vishakhedu@gmail.com>
2026-04-09 17:01:15 -0400

    Refactor buildKeywordList by removing string processing
    Removed unnecessary string manipulation from the buildKeywordList function.

--- src/educationPortalChat.js
+++ src/educationPortalChat.js
@@ -563,9 +563,7 @@ Please answer the user using the transcript context.`
       500
     );
   }
-}    .replace(/[^\\w\\s]/g, " ")
-    .split(/\\s+/)
-    .filter(Boolean);
+}   
 
   const stop = new Set([
     "the","a","an","and","or","but","if","then","than","so","of","for","to","in","on","at","by",
@@ -589,7 +587,7 @@ Please answer the user using the transcript context.`
   }
 
   return keywords;
-}
+
 
 async function fetchRelevantRows(env, question, history) {
   const keywords = buildKeywordList(question, history);
